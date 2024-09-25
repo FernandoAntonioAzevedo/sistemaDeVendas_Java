b91e2673892df73c2fb7d85cbc59d391ff5652cc
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2024-09-25 14:43:51 -0300

    bug de apresentação da lista de Cidades corrigido

--- SistemaVendas/src/main/java/com/sistemaDeVendas/sistema/repositorios/CidadeRepositorio.java
+++ SistemaVendas/src/main/java/com/sistemaDeVendas/sistema/repositorios/CidadeRepositorio.java
@@ -2,8 +2,8 @@ package com.sistemaDeVendas.sistema.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.sistemaDeVendas.sistema.modelos.Estado;
+import com.sistemaDeVendas.sistema.modelos.Cidade;
 
-public interface CidadeRepositorio extends JpaRepository<Estado, Long> {
+public interface CidadeRepositorio extends JpaRepository<Cidade, Long> {
 
 }
